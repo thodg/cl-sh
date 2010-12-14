d5a93534a8bb7c1ab64b6cefef2140f13548b023
Thomas de Grivel <billitch@gmail.com>
2010-12-14 04:10:17 +0100

    fixed license and probe-dir

--- test.lisp
+++ test.lisp
@@ -1,43 +1,26 @@
 ;;
-;;  CL-WHOIS  -  WHOIS Protocol in Common Lisp
+;;  CL-SH  -  Unix Shell in Common Lisp
 ;;
 ;;  Copyright 2010 Thomas de Grivel <billitch@gmail.com>
 ;;
-;;  Permission is hereby granted, free of charge, to any person
-;;  obtaining a copy of this software and associated documentation
-;;  files (the "Software"), to deal in the Software without
-;;  restriction, including without limitation the rights to use, copy,
-;;  modify, merge, publish, distribute, sublicense, and/or sell copies
-;;  of the Software, and to permit persons to whom the Software is
-;;  furnished to do so, subject to the following conditions:
-;;  
-;;  The above copyright notice and this permission notice shall be
-;;  included in all copies or substantial portions of the Software.
+;;  Permission to use, copy, modify, and distribute this software for any
+;;  purpose with or without fee is hereby granted, provided that the above
+;;  copyright notice and this permission notice appear in all copies.
 ;;
-;;  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-;;  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-;;  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-;;  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-;;  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-;;  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;;  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-;;  DEALINGS IN THE SOFTWARE.
+;;  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+;;  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+;;  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+;;  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+;;  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+;;  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+;;  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ;;
 
 (in-package :cl-sh)
 
+
 (defun probe-dir (pathname)
-  (when (pathname-name pathname)
-    (warn "(PROBE-DIR ~A) : pathname is not a directory" pathname))
-  (loop
-     with dir = (pathname-directory pathname)
-     for i from 1 upto (length dir)
-     do (let ((newpath (make-pathname :host (pathname-host pathname)
-				      :device (pathname-device pathname)
-				      :directory (subseq dir 0 i))))
-	  (unless (probe-file newpath)
-	    (return nil)))
-     finally (return t)))
+  (sb-posix:s-isdir (sb-posix:stat-mode (sb-posix:stat pathname))))
 
 (defun test-1 (op arg)
   (ecase op
